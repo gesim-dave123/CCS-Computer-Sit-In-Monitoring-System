7c956787738256634d0b0d3ad61a7390d79e7aae
unknown <TT625@ccs.local>
2026-02-28 18:34:44 +0800

    REFACTOR: separated the register modal

---
+++ client/src/pages/register.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import RegisterModal from "../component/modals/registerModal";
+import { useNavigate } from "react-router-dom";
+import csslogo from "../assets/image/ccslogo.png";
+
+export default function RegisterPage() {
+  const [modal, setModal] = useState("login");
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <img
+            src={csslogo}
+            alt=""
+            className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none select-none"
+             style={{ width: 1040, height: 1040, opacity: 0.4, zIndex: 0 }}
+          />
+        <RegisterModal
+          onClose={() => {
+            setModal(null);
+            navigate("/");
+          }}
+          onSwitchToLogin={() => setModal("login")}
+        />
+    </>
+  );
+}
