b95a754f02f3a5e1a7dae3c464f318bafb00ef60
unknown <TT625@ccs.local>
2026-02-28 17:50:47 +0800

    feat: updated layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/landingPage";
 import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/register";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
