ffd0314adeab3e7aa9e5dd43a186314223e200fd
gesim-dave123 <christiandavegesim@gmail.com>
2026-03-21 02:57:54 +0800

    Fix: Removed the a line of coe that echo a message , disrupting the database connection

--- server/config/db_connection.php
+++ server/config/db_connection.php
@@ -8,7 +8,7 @@
         $pdo = new PDO("mysql:host=$sName;dbname=$db_name;charset=utf8mb4", $uName, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        echo "Connected successfully";
+
     } catch (PDOException $e) {
         http_response_code(500);
         echo json_encode(["error" => "Connection failed: " . $e->getMessage()]);
